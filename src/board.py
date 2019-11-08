753d9df0717ed5152d38f9236221ddbe2a08ef37
Omar Torres <torresomarmx@gmail.com>
2019-11-08 11:40:41 -0500

    add functions for mapping files and ranks to positions in internal grid

--- src/board.py
+++ src/board.py
@@ -6,8 +6,8 @@ class Board:
         # rotates boars 180 degrees, so that black pieces are at the bottom
         self.rotated = rotated
         self.board = self.__create()
-        self.rank_mapping = self.__create_rank_mapping()
-        self.file_mapping = self.__create_file_mapping()
+        self.rank_to_xindex_mapping = self.__create_rank_to_xindex_mapping()
+        self.file_to_yindex_mapping = self.__create_file_to_yindex_mapping()
 
     def __create(self):
         board = self.__create_empty_grid()
@@ -47,13 +47,21 @@ class Board:
                 for i in range(len(starter_ranks[1])):
                     starter_ranks[1][i] = "PB"
 
-    def __create_rank_mapping(self):
+    def __create_rank_to_xindex_mapping(self):
         # this maps a chess rank position (A, B, C, etc..) to a X position on the 8x8 grid
+        ranks = [chr(i).upper() for i in range(97, 105)]
         if self.rotated:
+            return {rank: index for index, rank in enumerate(ranks[::-1])}
+        else:
+            return {rank: index for index, rank in enumerate(ranks)}
 
-    def __create_file_mapping(self):
+    def __create_file_to_yindex_mapping(self):
         # this maps a chess file position (1, 2, 3, et..) to a Y position on the 8x8 grid
+        files = [str(i) for i in range(1, 9)]
         if self.rotated:
+            return {file: index for index, file in enumerate(files[::-1])}
+        else:
+            return {file: index for index, file in enumerate(files)}
 
     def display(self):
         for rank in self.board:
